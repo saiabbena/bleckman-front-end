08896e5f1384b6e377e15fb272dfd05ee4dcbcfa
Lokesh <lokeshdas248@gmail.com>
2016-11-17 14:33:11 +0530

    Return reason validation message

--- js/script.js
+++ js/script.js
@@ -311,6 +311,7 @@ $(document).ready(function(){
     
     if($('#override > div.container-fluid.form2 input[type="checkbox"]:checked').length==0){
       setTimeout(function(){
+		  $('#screen2-error').html('<strong>Whoops!</strong>  Please select at least one item to return.');
         $('#screen2-error').show();
       }, 500);
       $('.loading-screen').slideUp('slow');
@@ -330,7 +331,10 @@ $(document).ready(function(){
       if ( reason == '0' ) {
   		  setTimeout(function(){
     			$('#s1', parent).focus();			
-    			$('#reason_error').show();
+    			//$('#reason_error').show();
+				$('#screen2-error').html('<strong>Whoops!</strong>  Please select the return reason.');
+				$('#screen2-error').show();
+				
   			}, 500);
   			$('.loading-screen').slideUp('slow');
   			throw new Error("whoops");
