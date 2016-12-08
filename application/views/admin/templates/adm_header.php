a60b5c11b226ae9fae766558ac0390b8183056f3
Lokesh <lokeshdas248@gmail.com>
2016-12-08 11:33:14 +0530

    no message

--- application/views/admin/templates/adm_header.php
+++ application/views/admin/templates/adm_header.php
@@ -126,7 +126,7 @@
 		  <a href='ro_option'>
           <div  <?php if($this->uri->segment(2) == 'ro_option'){?>style='background-color: #009688; color: #fff;'<?php }?> class='col-md-offset-0 col-md-12 col-xs-2 text-left bm-nav-center'>
             <div class='hidden-lg hidden-md'></div>
-            <img src='<?php echo base_url();?>img/i-17.png' class='menu-icon' height='20px'> <span class='hidden-xs hidden-sm'>Return Order Option</span>
+            <img src='<?php echo base_url();?>img/i-17.png' class='menu-icon' height='20px'> <span class='hidden-xs hidden-sm'>Return Orders</span>
           </div>
           </a>
 		  <!--<a href='operation_mode'>
