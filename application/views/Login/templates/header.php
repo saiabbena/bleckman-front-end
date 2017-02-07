31dbda955fc56624a1e2e900c351d22108e7ed3f
sangeethaPelluri <geetha.852@gmail.com>
2017-02-07 12:24:31 +0530

    latest update from dev server branch

--- application/views/Login/templates/header.php
+++ application/views/Login/templates/header.php
@@ -28,7 +28,16 @@
     <!--My assets-->
     <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" />
     <script type="text/javascript" src="<?php echo base_url();?>js/scriptadmin2.js"></script>
-    
+    <script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-64703754-4', 'auto');
+		ga('send', 'pageview');
+
+	</script>
   </head>
   <body id='override'>
     <div class="bm-h">
