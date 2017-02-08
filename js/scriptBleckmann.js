ef12441301771d8ae1bf90e87dfa8abd08121eb1
sangeethaPelluri <geetha.852@gmail.com>
2017-02-08 13:38:39 +0530

    test UAT update

--- js/scriptBleckmann.js
+++ js/scriptBleckmann.js
@@ -1146,6 +1146,7 @@ $(document).ready(function() {
 		});
 		//$('div#showError').html('');
 		$(document).on('click','.generateLabel', function() {
+			//Test UAT update
 		    $('.loading-screen').slideDown('slow');   
 		    var retArray = $(this).attr("id").split("-");
 		    var apiCall=url+'returnorder/PostReturnLabelByReturnId';
